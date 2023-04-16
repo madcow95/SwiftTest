cb9cfcd332997d9652305808746c3ece13b28711
madcow95 <lobasketve@naver.com>
2023-04-16 20:47:14 +0900

    first commit

---
+++ HealthRecord/HealthRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthRecordApp.swift
+//  HealthRecord
+//
+//  Created by ChoiKwangWoo on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct HealthRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
