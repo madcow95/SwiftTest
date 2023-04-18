c3b067af5d83dddd4ce35be38eebc36ed67720a0
madcow95 <lobasketve@naver.com>
2023-04-18 21:39:32 +0900

    불필요 코드 삭제

--- HealthRecord/Record/RecordView.swift
+++ HealthRecord/Record/RecordView.swift
@@ -16,16 +16,12 @@ struct RecordView: View {
     
     var body: some View {
         List {
-            ForEach(chatList, id: \.self) { chat in
+            ForEach(chatList) { chat in
                 HStack {
                     Image(systemName: "macpro.gen1.fill")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 80, height: 80)
-                        .onTapGesture {
-                            showModal = true
-                            selectedItem = chat
-                        }
                         
                     Spacer()
                     
@@ -37,6 +33,10 @@ struct RecordView: View {
                     Spacer()
                     
                     Text(chat.date)
+                }
+                .onTapGesture {
+                    showModal = true
+                    selectedItem = chat
                 }
             }
             .sheet(item: $selectedItem) { item in
